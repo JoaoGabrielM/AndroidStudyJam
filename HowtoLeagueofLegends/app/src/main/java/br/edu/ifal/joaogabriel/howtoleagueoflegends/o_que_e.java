0ee3511a20996f6b3cb2b0af82195966b2d0ab4c
unknown <unknown@unknown.com>
2016-03-12 11:33:50 -0300

    Criação da segunda activity
    Criação da activity "O que é"

--- HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/o_que_e.java
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/o_que_e.java
@@ -3,13 +3,11 @@ package br.edu.ifal.joaogabriel.howtoleagueoflegends;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class o_que_e extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_o_que_e);
     }
-
-    //public void
 }
