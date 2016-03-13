6b7b928898cbef6bb799e1e97942cd7293fe3cf3
João Gabriel Machado <joaogabriel013@gmail.com>
2016-03-13 10:18:33 -0300

    Tentativa de alterar o layout

--- HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/o_que_e.java
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/o_que_e.java
@@ -1,5 +1,6 @@
 package br.edu.ifal.joaogabriel.howtoleagueoflegends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class o_que_e extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_o_que_e);
+
+        Intent it = getIntent();
     }
 }
