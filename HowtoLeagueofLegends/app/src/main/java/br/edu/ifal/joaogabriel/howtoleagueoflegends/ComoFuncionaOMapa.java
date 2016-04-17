942347d2975cf2bb9f6d34dad4e1d6b7ba82e2cc
João Gabriel Machado <joaogabriel013@gmail.com>
2016-04-17 13:10:57 -0300

    Criação da Acitivity Quadrantes e outras alterações

--- HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/ComoFuncionaOMapa.java
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/ComoFuncionaOMapa.java
@@ -1,7 +1,9 @@
 package br.edu.ifal.joaogabriel.howtoleagueoflegends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ComoFuncionaOMapa extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ComoFuncionaOMapa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_como_funciona_o_mapa);
+    }
+
+    public void chamarProximaPagina(View view){
+        Intent it = new Intent(this, Quadrantes.class);
+
+        startActivity(it);
     }
 }
