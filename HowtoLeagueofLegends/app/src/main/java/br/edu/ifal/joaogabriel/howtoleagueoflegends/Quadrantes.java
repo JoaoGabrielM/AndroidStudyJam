e107e6a2488b337429af167e6ae2332c1b106294
João Gabriel Machado <joaogabriel013@gmail.com>
2016-04-17 13:31:23 -0300

    Criação dos quatro quadrantes

--- HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/Quadrantes.java
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/Quadrantes.java
@@ -1,7 +1,9 @@
 package br.edu.ifal.joaogabriel.howtoleagueoflegends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Quadrantes extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class Quadrantes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quadrantes);
+    }
+
+    public void chamarPrimeiroQuadrante(View view){
+        Intent it = new Intent(this, PrimeiroQuadrante.class);
+
+        startActivity(it);
+    }
+
+    public void chamarSegundoQuadrante(View view){
+        Intent it = new Intent(this, SegundoQuadrante.class);
+
+        startActivity(it);
+    }
+
+    public void chamarTerceiroQuadrante(View view){
+        Intent it = new Intent(this, TerceiroQuadrante.class);
+
+        startActivity(it);
+    }
+
+    public void chamarQuartoQuadrante(View view){
+        Intent it = new Intent(this, QuartoQuadrante.class);
+
+        startActivity(it);
     }
 }
---
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/TerceiroQuadrante.java
@@ -0,0 +1,13 @@
+package br.edu.ifal.joaogabriel.howtoleagueoflegends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TerceiroQuadrante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terceiro_quadrante);
+    }
+}
