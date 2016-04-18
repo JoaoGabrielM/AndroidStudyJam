11955ffe9229d5c5d22a6948161cad799103063e
João Gabriel Machado <joaogabriel013@gmail.com>
2016-04-18 19:42:59 -0300

    Finalização do segundo quadrante

--- HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/PrimeiroQuadrante.java
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/PrimeiroQuadrante.java
@@ -1,7 +1,9 @@
 package br.edu.ifal.joaogabriel.howtoleagueoflegends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PrimeiroQuadrante extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class PrimeiroQuadrante extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primeiro_quadrante);
+    }
+
+    public void chamarSegundoQuadrante(View view){
+        Intent it = new Intent(this, SegundoQuadrante.class);
+
+        startActivity(it);
     }
 }
--- HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/SegundoQuadrante.java
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/SegundoQuadrante.java
@@ -1,7 +1,9 @@
 package br.edu.ifal.joaogabriel.howtoleagueoflegends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SegundoQuadrante extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SegundoQuadrante extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo_quadrante);
+    }
+
+    public void chamarTerceiroQuadrante(View view){
+        Intent it = new Intent(this, TerceiroQuadrante.class);
+
+        startActivity(it);
     }
 }
