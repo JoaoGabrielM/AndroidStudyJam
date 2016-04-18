a9aed30838503c2bf578e3005883c716a28c2760
João Gabriel Machado <joaogabriel013@gmail.com>
2016-04-18 20:14:51 -0300

    Finalização do quarto quadrante
    E uns ajustes no terceiro quadrante

--- HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/QuartoQuadrante.java
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/QuartoQuadrante.java
@@ -1,7 +1,9 @@
 package br.edu.ifal.joaogabriel.howtoleagueoflegends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class QuartoQuadrante extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class QuartoQuadrante extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quarto_quadrante);
+    }
+
+    public void chamarPaginaQuadrantes(View view){
+        Intent it = new Intent(this, Quadrantes.class);
+
+        startActivity(it);
     }
 }
