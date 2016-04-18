bb3f1026ab32dec7b591229a8f3abc2b93b280fc
João Gabriel Machado <joaogabriel013@gmail.com>
2016-04-18 20:00:25 -0300

    Término do terceiro quadrante

--- HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/TerceiroQuadrante.java
+++ HowtoLeagueofLegends/app/src/main/java/br/edu/ifal/joaogabriel/howtoleagueoflegends/TerceiroQuadrante.java
@@ -1,7 +1,9 @@
 package br.edu.ifal.joaogabriel.howtoleagueoflegends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TerceiroQuadrante extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class TerceiroQuadrante extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terceiro_quadrante);
+    }
+
+    public void chamarQuartoQuadrante(View view){
+        Intent it = new Intent(this, QuartoQuadrante.class);
+
+        startActivity(it);
     }
 }
